315ba88fb0db0bb6eca1e6a8c971e1a8cdc349e0
Mya316 <mybagismissing@gmail.com>
2025-01-16 06:21:26 +0000

    updates

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print ('Welcome to the Plushy Factory Chatbot')
-name = input('What will you name your plushy? ')
+print ('Welcome to the Plushie Factory Chatbot!')
+name = input('What will you name your plushie? ')
+age = input('Wow! ' + name + ' is such a great name! How old is ' + name + '? ')
